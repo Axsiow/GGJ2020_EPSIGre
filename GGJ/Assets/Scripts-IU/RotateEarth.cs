4c0b49b338778eca4a376996d97c40adbc4b3446
Axsiow <Axsiow@gmail.com>
2020-01-31 23:48:24 +0100

    Script rotate Earth

---
+++ GGJ/Assets/Scripts-IU/RotateEarth.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Script qui fait faire tourner la terre sur elle même automatiquement
+
+public class RotateEarth : MonoBehaviour {
+
+    public float Angle;
+
+    // Update is called once per frame
+    void Update () {
+        this.transform.Rotate ( 0, Angle, 0, Space.Self);
+    }
+}
