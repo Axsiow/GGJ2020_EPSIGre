9bf2f2e0d03b63507d7b428e03a1aea8e5be0ff0
manfredhq <hugo.quitton@gmail.com>
2020-01-31 21:43:05 +0100

    made the basics of the zones

--- GGJ/Assets/Scripts-zones/Zones.cs
+++ GGJ/Assets/Scripts-zones/Zones.cs
@@ -4,15 +4,44 @@ using UnityEngine;
 
 public class Zones : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+
+    public List<ICatastrophe> CatastrophesList;
+    public float TimeBetweenCatastrophe;
+    public float NextTimeCatastrophes;
+
+    private ICatastrophe CurrentCatastrophe;
+    private float TimeCatastropheEnd;
+
+    private void Start()
     {
-        
+        CurrentCatastrophe = null;
+        NextTimeCatastrophes = Time.time + TimeBetweenCatastrophe;
     }
 
-    // Update is called once per frame
-    void Update()
+
+    void FixedUpdate()
     {
-        
+        if (TimeBetweenCatastrophe <= Time.time && CurrentCatastrophe == null)
+        {
+            StartCatastrophe();
+        }
+
+        if (TimeCatastropheEnd <= Time.time && CurrentCatastrophe != null)
+        {
+            TakeDamage();
+        }
+    }
+
+
+    public void StartCatastrophe()
+    {
+        CurrentCatastrophe = CatastrophesList[Random.Range(0, CatastrophesList.Count)];
+        CurrentCatastrophe.LaunchCatastrophe();
+        TimeCatastropheEnd = Time.time + CurrentCatastrophe.Timer;
+    }
+    public void TakeDamage()
+    {
+        CurrentCatastrophe.StopCatastrophe();
+        //TODO : make the bigger entity (holding the hp) taking damage
     }
 }
