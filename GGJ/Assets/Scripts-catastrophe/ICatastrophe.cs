88aab10570ff6caf8ec79fa16f0e9ef54f0f707b
Unknown <wolfla@hotmail.fr>
2020-01-31 21:27:37 +0100

    AJout Enum et Interface catastrophe

---
+++ GGJ/Assets/Scripts-catastrophe/ICatastrophe.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface ICatastrophe
+{
+    ECatastrophe Type { get; set; }
+
+    ECatastrophe MoinsCatastrophe { get; set; }
+
+    ECatastrophe PlusCatastrophe { get; set; }
+
+    float Timer { get; set; }
+
+    void LaunchCatastrophe();
+
+    void StopCatastrophe();
+
+    void DeclenchePlusCatastrophe();
+
+    void DeclencheMoinsCatastrophe();
+}
