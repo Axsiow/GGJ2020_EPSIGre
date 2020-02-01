1f7d35142fe493e2da9ca37275ceebe5aa830a69
hugo <hugo.quitto@gmail.com>
2020-02-01 16:59:00 +0100

    add logique to button

---
+++ GGJ/Assets/Scripts-Manager/QuitApplication.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+#if UNITY_EDITOR
+using UnityEditor;
+
+#else
+using UnityEngine;
+#endif
+public class QuitApplication : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<Button>().onClick.AddListener(Quit);
+    }
+
+    // Update is called once per frame
+    private void Quit()
+    {
+#if UNITY_EDITOR
+        EditorApplication.isPlaying = false;
+#else
+            Application.Quit();
+#endif
+    }
+}
+
