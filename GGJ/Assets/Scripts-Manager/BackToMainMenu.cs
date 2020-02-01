6f7fcc33b567977637a2a33002f9945c26722639
hugo <hugo.quitto@gmail.com>
2020-02-01 16:31:19 +0100

    add win and loose scenes

---
+++ GGJ/Assets/Scripts-Manager/BackToMainMenu.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class BackToMainMenu : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<Button>().onClick.AddListener(Back);
+    }
+
+    private void Back()
+    {
+        SceneManager.LoadScene("Menu-Scene");
+    }
+}
