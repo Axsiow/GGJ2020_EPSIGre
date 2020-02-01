588ad3cbe120df0c4cf5081a4028d9c6cbdbac89
Axsiow <Axsiow@gmail.com>
2020-02-01 17:38:51 +0100

    Scenes levels finish

---
+++ GGJ/Assets/Scripts-IU/MoonRotate.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoonRotate : MonoBehaviour {
+    // Start is called before the first frame update
+    public float IddleRotationSpeed;
+    public bool IsRotating = true;
+
+    // Update is called once per frame
+    void Update () {
+        if (IsRotating) {
+            this.transform.Rotate (0, IddleRotationSpeed, 0, Space.Self);
+        }
+    }
+}
