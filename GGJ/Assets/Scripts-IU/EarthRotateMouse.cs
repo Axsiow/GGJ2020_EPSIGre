c223372836e332507da8b88812744e5e6ccfd0bd
manfredhq <hugo.quitton@gmail.com>
2020-02-01 01:55:24 +0100

    fixezd a strange bug with script

---
+++ GGJ/Assets/Scripts-IU/EarthRotateMouse.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EarthRotateMouse : MonoBehaviour
+{
+    public float maxSpeed;
+
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            float rotationX = Input.GetAxis("Mouse X") * maxSpeed * Mathf.Deg2Rad;
+            transform.RotateAround(Vector3.up, -rotationX);
+        }
+    }
+}
