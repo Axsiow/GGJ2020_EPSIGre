a1573165fa1b7e91888e9eceaa839b24086b226b
manfredhq <hugo.quitton@gmail.com>
2020-02-01 02:29:16 +0100

    added the planet manager

---
+++ GGJ/Assets/Scripts-Manager/PlanetManager.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlanetManager : MonoBehaviour
+{
+    public int hp;
+    public List<Zones> zoneList;
+
+    public void TakeDamage()
+    {
+        hp -= 1;
+    }
+}
