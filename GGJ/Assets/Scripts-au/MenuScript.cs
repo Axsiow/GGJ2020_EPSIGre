b628755a435da87c689ceefad3a06886af31feb5
Théo <theo_38180@live.fr>
2020-02-01 18:35:11 +0100

    Menus

---
+++ GGJ/Assets/Scripts-au/MenuScript.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MenuScript : MonoBehaviour
+{
+    public Button boutonStart;
+    public Button boutonAbout;
+    public Button boutonHtp;
+    public Button boutonQuit;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        boutonStart.onClick.AddListener(changesceneStart);
+        boutonAbout.onClick.AddListener(changesceneAbout);
+        boutonHtp.onClick.AddListener(changesceneHtp);
+        boutonQuit.onClick.AddListener(changesceneQuit);
+    }
+
+    private void changesceneStart()
+    {
+        SceneManager.LoadScene("LevelSelect-Scene");
+    }
+
+    private void changesceneAbout()
+    {
+        SceneManager.LoadScene("About-Scene");
+    }
+
+    private void changesceneHtp()
+    {
+        SceneManager.LoadScene("Htp-Scene");
+    }
+
+    private void changesceneQuit()
+    {
+        Application.Quit();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
